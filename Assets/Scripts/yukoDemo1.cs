5e583df77ded44ed2f87ade44aaa4c17b0e167f0
WIN8THS40779\Ando Hiroki <ashsavior17@gmail.com>
2017-10-17 16:12:17 +0900

    follow/lookat設定なしで、レールの上を一定速度で移動するサンプルシーンを追加

--- Assets/Scripts/yukoDemo1.cs
+++ Assets/Scripts/yukoDemo1.cs
@@ -6,17 +6,20 @@ public class yukoDemo1 : MonoBehaviour {
 
     private Animator animator;
     private bool Active;//updateの有効化on/off
+    private int cnt = 0;
     // Use this for initialization
     void Start()
     {
         Active = true;
         animator = GetComponent<Animator>();
+        cnt = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Active == true && Input.GetKey("up"))
+        cnt++;
+        if (cnt <= 400)
         {
             transform.position += transform.forward * 0.08f;
             animator.SetBool("is_running", true);
@@ -25,14 +28,24 @@ public class yukoDemo1 : MonoBehaviour {
         {
             animator.SetBool("is_running", false);
         }
-        if (Active == true && Input.GetKey("right"))
-        {
-            transform.Rotate(0, 3, 0);
-        }
-        if (Active == true && Input.GetKey("left"))
-        {
-            transform.Rotate(0, -3, 0);
-        }
+
+        //if (Active == true && Input.GetKey("up"))
+        //{
+        //    transform.position += transform.forward * 0.08f;
+        //    animator.SetBool("is_running", true);
+        //}
+        //else
+        //{
+        //    animator.SetBool("is_running", false);
+        //}
+        //if (Active == true && Input.GetKey("right"))
+        //{
+        //    transform.Rotate(0, 3, 0);
+        //}
+        //if (Active == true && Input.GetKey("left"))
+        //{
+        //    transform.Rotate(0, -3, 0);
+        //}
     }
     public void PlayerActive(bool flag)
     {
